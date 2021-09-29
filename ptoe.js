b9dd72b24e39b828ae0d5e635767cebcc86369a6
mo linares <91020011+linares-m@users.noreply.github.com>
2021-09-29 13:49:24 -0500

    Update ptoe.js

--- ptoe.js
+++ ptoe.js
@@ -1,18 +1,26 @@
 window.addEventListener("DOMContentLoaded", elInfo)
 const selector = document.getElementsByTagName('g');
-  function elInfo(){
-    for (i=0; i < selector.length; i++){
+function elInfo(){
+  for (i=0; i < selector.length; i++){
 
-      selector[i].addEventListener("click", function(){
-       document.querySelector("#infoReader").innerHTML = "About This Element: " +  this.dataset.summary
-      })
-    }
+    selector[i].addEventListener("click", function(){
+      let datum = []
+      datum = this.dataset
+      document.querySelector("#infoReader").innerHTML = "Name: " + datum.name + ",  Number: " + datum.number + ",     Category: " + datum.category + ",   Atomic Mass: " + datum.atomicMass + ",  Summary: " +  datum.summary + "   Source: "  + datum.source
+      d3.select(".infoPane")
+      .selectAll("p")
+      .data(datum)
+      .enter().append("p")
+      .text('test')
+      .style("margin-top", "8px" )
+    })
   }
+}
 
 /*window.addEventListener("DOMContentLoaded", createCatMenu)
 function createCatMenu(){
-   var gNodes = document.querySelectorAll("g")
-   for (i = 0; i < gNodes.length; i++){
-      console.log(gNodes[i].dataset.category)
-   }
+var gNodes = document.querySelectorAll("g")
+for (i = 0; i < gNodes.length; i++){
+console.log(gNodes[i].dataset.category)
+}
 }*/
