989968090432fb823dff78169e3b013581f9956b
mo linares <91020011+linares-m@users.noreply.github.com>
2021-09-30 15:24:55 -0500

    Cleaner display

--- ptoe.js
+++ ptoe.js
@@ -1,3 +1,8 @@
+//TODO:
+// Add link in place of source
+// Category selections
+//
+
 window.addEventListener("DOMContentLoaded", elInfo)
 const selector = document.getElementsByTagName('g');
 function elInfo(){
@@ -6,11 +11,14 @@ function elInfo(){
     selector[i].addEventListener("click", function(){
       let datum = []
       datum = this.dataset
+      document.querySelector("#iRI").style.display = "none";
       document.querySelector("#iR1").innerHTML = "Name: " + datum.name + ", Number: " + datum.number + ", Category: " + datum.category + ", Phase: " + datum.phase + ", Appearance: " + datum.appearance;
       document.querySelector("#iR2").innerHTML = "Atomic Mass: " + datum.atomicMass + ",  Density: " + datum.density + ", Melting Point: " + datum.meltingPoint + ", Boiling Point: " + datum.boilingPoint, ", Electron Affinity: " + datum.electronConfig;
-      document.querySelector("#iR3").innerHTML = "Electron Shells" + datum.shells;
-      document.querySelector("#iR4").innerHTML =   "Summary: " +  datum.summary;
-      document.querySelector("#iR5").innerHTML =  "Source: "  + datum.source;
+      document.querySelector("#iR3").innerHTML =  datum.shells;
+      document.querySelector("#iR4").innerHTML =    datum.summary;
+      document.querySelector("#iR5").setAttribute("href", datum.source);
+      document.querySelector("#iRD").style.visibility = "visible";
+
     })
   }
 }
